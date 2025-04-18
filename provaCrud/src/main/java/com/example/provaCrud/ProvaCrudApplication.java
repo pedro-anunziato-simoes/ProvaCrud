6b5f37bbef724c5ec8cbd40dadc7cacf1cffc6e8
Pedro Henrique Da Silva Gomes <pedro.anunziato.simoes@gmail.com>
2025-04-17 21:48:51 -0300

    primeiro e ultimo commit

---
+++ provaCrud/src/main/java/com/example/provaCrud/ProvaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaCrudApplication.class, args);
+	}
+
+}
---
+++ provaCrud/src/test/java/com/example/provaCrud/ProvaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
